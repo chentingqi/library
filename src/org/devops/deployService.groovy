9771614db939ebaacc46bf6d793e60a53f8637c0
陈景涛 <cjt@youlu.com>
2020-10-09 15:05:52 +0800

    Add new file

---
+++ src/org/devops/deployService.groovy
@@ -0,0 +1,26 @@
+package org.devops
+
+def deployService(deploytype,deploypackage,deployip,srcdir,deploydir,deployshell){
+    if (deploytype == 'ssh'){
+       try{
+          sh """"
+             scp ${deploypackage} ${deployip}:/${deploydir}
+             ssh -p 22 ${deployip} 'sh ${deployshell}'
+             """"
+       }catch (e){
+            currentBuild.description="服务部署失败!"
+            error '服务部署失败!'
+        }
+    }
+    if (deploytype == 'salt'){
+       try{
+          sh """"
+             salt ${deployip} cp.get_file salt://${srcdir}/${deploypackage} ${deploydir}
+             salt ${deployip} cmd.run 'sh ${deployshell}'
+             """"
+       }catch (e){
+            currentBuild.description="服务部署失败!"
+            error '服务部署失败!'
+        }
+    }
+}
