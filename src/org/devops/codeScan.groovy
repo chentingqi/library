2b40aa5ee602f5847b084868a713f8b68e849984
陈景涛 <cjt@youlu.com>
2020-10-09 14:20:07 +0800

    Update codeScan.groovy

--- src/org/devops/codeScan.groovy
+++ src/org/devops/codeScan.groovy
@@ -22,11 +22,10 @@ def codeScan(projectType,srcDir,serviceName){
         try {
             sh """
                 cd ${srcDir} 
-                ${scanHome}/bin/sonar-scanner -X -Dsonar.projectName=${serviceName} -Dsonar.projectKey=${serviceName}  \
-                -Dsonar.sources=.  -Dsonar.language=c -Dsonar.sourceEncoding=UTF-8 \
-                -Dsonar.java.binaries=${serviceName} -Dsonar.java.coveragePlugin=jacoco \
-                -Dsonar.jacoco.reportPath=target/jacoco.exec -Dsonar.junit.reportsPath=target/surefire-reports \
-                -Dsonar.surefire.reportsPath=target/surefire-reports -Dsonar.projectDescription='devopsdevops' -Dsonar.host.url=http://192.168.10.83:9000
+                cp /data/build-devops/sonar-project.properties ${srcDir}
+                sed -i 's/jobname/${serviceName}/g' sonar-project.properties
+                sed -i 's/sonar.language=java/sonar.language=c/g' sonar-project.properties
+                ${scanHome}/bin/sonar-scanner -X -Dsonar.host.url=http://192.168.10.83:9000
              """ 
         } catch (e){
             currentBuild.description="代码扫描失败!"
@@ -37,9 +36,11 @@ def codeScan(projectType,srcDir,serviceName){
     else if (projectType == 'web'){
         try {
             sh  """
-                cd ${srcDir} 
-                ${scanHome}/bin/sonar-scanner -Dsonar.projectName=${serviceName} \
-                -Dsonar.projectKey=${serviceName} -Dsonar.sources=${scanDir} -Dsonar.language=js  
+                cd ${srcDir}
+                cp /data/build-devops/sonar-project.properties ${srcDir}
+                sed -i 's/jobname/${serviceName}/g' sonar-project.properties
+                sed -i 's/sonar.language=java/sonar.language=js/g' sonar-project.properties
+                ${scanHome}/bin/sonar-scanner -X -Dsonar.host.url=http://192.168.10.83:9000  
                 cd - 
                 """
         } catch (e){
