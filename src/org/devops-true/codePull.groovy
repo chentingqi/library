699b1e5ac7579a36e519f66ffe76f1cb02f6d8e2
陈景涛 <l>
2020-10-09 14:23:05 +0800

    新增测试环境配置

---
+++ src/org/devops-true/codePull.groovy
@@ -0,0 +1,29 @@
+package src.org.devops
+
+def gitPull(giturl,gitbranch,gitcert){
+//    git branch: 'gitbranch', credentialsId: 'gitcert', url: 'giturl'
+      checkout([$class: 'GitSCM', branches: [[name: "${gitbranch}"]], 
+                doGenerateSubmoduleConfigurations: false, 
+                extensions: [], submoduleCfg: [], 
+                userRemoteConfigs: [[credentialsId: "${gitcert}", 
+                url: "${giturl}"]]])
+}
+
+def svnPull(svn_url,svn_cert){
+    checkout([$class: 'SubversionSCM', 
+              additionalCredentials: [], 
+              excludedCommitMessages: '', 
+              excludedRegions: '', 
+              excludedRevprop: '', 
+              excludedUsers: '', 
+              filterChangelog: false, 
+              ignoreDirPropChanges: false, 
+              includedRegions: '', 
+              locations: [[cancelProcessOnExternalsFail: true, 
+              credentialsId: "${svn_cert}", 
+              depthOption: 'infinity', 
+              ignoreExternalsOption: true, local: '.', 
+              remote: "${svn_url}"]], 
+              quietOperation: true, 
+              workspaceUpdater: [$class: 'UpdateUpdater']])
+}
---
+++ src/org/devops-true/codeScan.groovy
@@ -0,0 +1,51 @@
+package org.devops
+
+
+//代码扫描
+def codeScan(projectType,srcDir,serviceName){
+    def scanHome = "/data/sonar-scanner-3.0.0.702-linux"
+    if (projectType == 'java'){
+        try {
+            sh """
+                cd ${srcDir} 
+                cp /data/build-devops/sonar-project.properties ${srcDir}
+                sed -i 's/jobname/${serviceName}/g' sonar-project.properties
+                ${scanHome}/bin/sonar-scanner -X -Dsonar.host.url=http://192.168.10.83:9000
+             """ 
+        } catch (e){
+            currentBuild.description="代码扫描失败!"
+            error '代码扫描失败!'
+        }
+    }
+    
+    if (projectType == 'c'){
+        try {
+            sh """
+                cd ${srcDir} 
+                cp /data/build-devops/sonar-project.properties ${srcDir}
+                sed -i 's/jobname/${serviceName}/g' sonar-project.properties
+                sed -i 's/sonar.language=java/sonar.language=c/g' sonar-project.properties
+                ${scanHome}/bin/sonar-scanner -X -Dsonar.host.url=http://192.168.10.83:9000
+             """ 
+        } catch (e){
+            currentBuild.description="代码扫描失败!"
+            error '代码扫描失败!'
+        }
+    }
+    
+    else if (projectType == 'web'){
+        try {
+            sh  """
+                cd ${srcDir}
+                cp /data/build-devops/sonar-project.properties ${srcDir}
+                sed -i 's/jobname/${serviceName}/g' sonar-project.properties
+                sed -i 's/sonar.language=java/sonar.language=js/g' sonar-project.properties
+                ${scanHome}/bin/sonar-scanner -X -Dsonar.host.url=http://192.168.10.83:9000  
+                cd - 
+                """
+        } catch (e){
+            currentBuild.description="代码扫描失败!"
+            error '代码扫描失败!'
+        }
+    }
+}
