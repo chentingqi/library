d219c793b34f60a6cf7470993b027bf3612e72c1
陈景涛 <cjt@youlu.com>
2020-10-09 15:51:00 +0800

    Update serviceCheck.groovy

--- src/org/devops/serviceCheck.groovy
+++ src/org/devops/serviceCheck.groovy
@@ -14,11 +14,11 @@ def serviceCheck(deploytype,deployip,jobname,servicename){
     if (deploytype == 'salt'){
        try{
           sh """
-             sh "sleep 5s"
-             sh "cp /data/build-devops/service_check.sh /data/jenkins_home/workspace/${jobname}/"
-             sh "cp /data/build-devops/sls/service_check.sls /data/salt/service_check-${jobname}.sls"
-             sh "sed -i 's/jarname/${servicename}/g'  service_check.sh"
-             sh "sed -i 's/jobname/${jobname}/g' /data/salt/service_check-${jobname}.sls"
+             sleep 5s
+             cp /data/build-devops/service_check.sh /data/jenkins_home/workspace/${jobname}/
+             cp /data/build-devops/sls/service_check.sls /data/salt/service_check-${jobname}.sls
+             sed -i 's/jarname/${servicename}/g'  service_check.sh
+             sed -i 's/jobname/${jobname}/g' /data/salt/service_check-${jobname}.sls
              salt ${deployip} state.sls service_check-${jobname}
              """
        }catch (e){
