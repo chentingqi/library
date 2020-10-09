1c016084c63c416351c8092f1e7358d7ecf7b777
陈景涛 <cjt@youlu.com>
2020-10-09 15:42:15 +0800

    Update logCheck.groovy

--- src/org/devops/logCheck.groovy
+++ src/org/devops/logCheck.groovy
@@ -1,6 +1,6 @@
 package org.devops
 
-def logCheck(deploytype,deployip,srcdir){
+def logCheck(deploytype,deployip,jobname,logname){
     if (deploytype == 'ssh'){
        try{
           sh """
@@ -14,7 +14,13 @@ def logCheck(deploytype,deployip,srcdir){
     if (deploytype == 'salt'){
        try{
           sh """
-             salt ${deployip} state.sls log_check-${srcdir}
+             sh "sleep 5s"
+             sh "cp /data/build-devops/log_check.sh /data/jenkins_home/workspace/${jobname}/"
+             sh "cp /data/build-devops/sls/log_check.sls /data/salt/log_check-${jobname}.sls"
+             sh "sed -i 's#log#${logname}#g' log_check.sh"
+             sh "sed -i 's/jobname/${jobname}/g' /data/salt/log_check-${jobname}.sls"
+
+             salt ${deployip} state.sls log_check-${jobname}
              """
        }catch (e){
             currentBuild.description="日志输出失败!"
