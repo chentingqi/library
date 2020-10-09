2e347993f4bb81adfb9909c5358499b9693f64c0
陈景涛 <cjt@youlu.com>
2020-10-09 14:10:59 +0800

    Update codeScan.groovy

--- src/org/devops/codeScan.groovy
+++ src/org/devops/codeScan.groovy
@@ -9,10 +9,8 @@ def codeScan(projectType,srcDir,serviceName){
             sh """
                 cd ${srcDir} 
                 ${scanHome}/bin/sonar-scanner -X -Dsonar.projectName=${serviceName} -Dsonar.projectKey=${serviceName}  \
-                -Dsonar.sources=.  -Dsonar.language=java -Dsonar.sourceEncoding=UTF-8 \
-                -Dsonar.java.binaries=${serviceName} -Dsonar.java.coveragePlugin=jacoco \
-                -Dsonar.jacoco.reportPath=target/jacoco.exec -Dsonar.junit.reportsPath=target/surefire-reports \
-                -Dsonar.surefire.reportsPath=target/surefire-reports -Dsonar.projectDescription='devopsdevops' -Dsonar.host.url=http://192.168.10.83:9000
+                -Dsonar.sources=${srcDir}  -Dsonar.language=java -Dsonar.sourceEncoding=UTF-8 \
+                -Dsonar.java.binaries=${serviceName}  -Dsonar.host.url=http://192.168.10.83:9000
              """ 
         } catch (e){
             currentBuild.description="代码扫描失败!"
