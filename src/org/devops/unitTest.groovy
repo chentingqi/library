b1f858ad0fefdcb5c1e200629251345d294a9f4f
陈景涛 <cjt@youlu.com>
2020-10-09 17:28:08 +0800

    Add new file

---
+++ src/org/devops/unitTest.groovy
@@ -0,0 +1,15 @@
+package org.devops
+
+def unitTest(){
+    try{
+       sh """
+          export MAVEN_HOME=/data/apache-maven-3.6.3
+          export PATH=\MAVEN_HOME/bin:\$PATH
+          mvn test
+       """
+    }catch (e){
+            currentBuild.description="单元测试失败!"
+            error '单元测试失败!'
+               }
+
+}
