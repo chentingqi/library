d5961431c1e7d6715c156b9330f565efa2815493
陈景涛 <cjt@youlu.com>
2020-10-09 15:24:04 +0800

    Update deployService.groovy

--- src/org/devops/deployService.groovy
+++ src/org/devops/deployService.groovy
@@ -3,10 +3,10 @@ package org.devops
 def deployService(deploytype,deploypackage,deployip,srcdir,deploydir,deployshell){
     if (deploytype == 'ssh'){
        try{
-          sh """"
+          sh """
              scp ${deploypackage} ${deployip}:/${deploydir}
              ssh -p 22 ${deployip} 'sh ${deployshell}'
-             """"
+             """
        }catch (e){
             currentBuild.description="服务部署失败!"
             error '服务部署失败!'
@@ -14,10 +14,10 @@ def deployService(deploytype,deploypackage,deployip,srcdir,deploydir,deployshell
     }
     if (deploytype == 'salt'){
        try{
-          sh """"
+          sh """
              salt ${deployip} cp.get_file salt://${srcdir}/${deploypackage} ${deploydir}
              salt ${deployip} cmd.run 'sh ${deployshell}'
-             """"
+             """
        }catch (e){
             currentBuild.description="服务部署失败!"
             error '服务部署失败!'
