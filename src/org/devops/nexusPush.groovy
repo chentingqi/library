df167b6721af7d89d26dee067a952a4229127baf
陈景涛 <cjt@youlu.com>
2020-10-13 17:41:05 +0800

    Update nexusPush.groovy

--- src/org/devops/nexusPush.groovy
+++ src/org/devops/nexusPush.groovy
@@ -1,11 +1,11 @@
 package org.devops
 
-def nexusPush(packagetype,jar,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusname2){
+def nexusPush(packagetype,package,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusname2){
     try{
           sh """
-            echo ${jar}
+            echo ${package}
             cp /data/build-devops/nexus-common/nexus-upload.sh $workspace
-            sed -i 's#name#${jar}#g' nexus-upload.sh
+            sed -i 's#name#${package}#g' nexus-upload.sh
             sed -i 's#cangku#${nexusRep}#g' nexus-upload.sh
             sed -i 's#group#${nexusGroup}#g' nexus-upload.sh
             sed -i 's#patch#${nexusPatch}#g' nexus-upload.sh
@@ -24,9 +24,9 @@ def nexusPush(packagetype,jar,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusnam
 def nexusTime(packagetype,package,nexusRep,nexusGroup,nexusname1,nexusname2){
     try{
           sh """
-            echo ${jar}
+            echo ${package}
             cp /data/build-devops/nexus-common/nexus-upload-time.sh $workspace
-            sed -i 's#name#${jar}#g' nexus-upload-time.sh
+            sed -i 's#name#${package}#g' nexus-upload-time.sh
             sed -i 's#cangku#${nexusRep}#g' nexus-upload-time.sh
             sed -i 's#group#${nexusGroup}#g' nexus-upload-time.sh
             sed -i 's/one/${nexusname1}/g' nexus-upload-time.sh
@@ -41,12 +41,12 @@ def nexusTime(packagetype,package,nexusRep,nexusGroup,nexusname1,nexusname2){
         }
 }
 
-def nexusTimeBook(packagetype,jar,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusname2,book){
+def nexusTimeBook(packagetype,package,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusname2,book){
     try{
           sh """
-            echo ${jar}
+            echo ${package}
             cp /data/build-devops/nexus-common/nexus-upload-time1.sh $workspace
-            sed -i 's#name#${jar}#g' nexus-upload-time1.sh
+            sed -i 's#name#${package}#g' nexus-upload-time1.sh
             sed -i 's#book#${book}#g' nexus-upload-time1.sh
             sed -i 's#cangku#${nexusRep}#g' nexus-upload-time1.sh
             sed -i 's#group#${nexusGroup}#g' nexus-upload-time1.sh
