233c8442d83814497e6365d60d0206c95656a624
陈景涛 <cjt@youlu.com>
2020-10-13 17:15:52 +0800

    Update nexusPush.groovy

--- src/org/devops/nexusPush.groovy
+++ src/org/devops/nexusPush.groovy
@@ -21,21 +21,41 @@ def nexusPush(packagetype,jar,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusnam
         }
 }
 
-def nexusTime(packagetype,jar,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusname2,book){
+def nexusTime(packagetype,package,nexusRep,nexusGroup,nexusname1,nexusname2){
     try{
           sh """
             echo ${jar}
             cp /data/build-devops/nexus-common/nexus-upload-time.sh $workspace
             sed -i 's#name#${jar}#g' nexus-upload-time.sh
-            sed -i 's#book#${book}#g' nexus-upload-time.sh
             sed -i 's#cangku#${nexusRep}#g' nexus-upload-time.sh
             sed -i 's#group#${nexusGroup}#g' nexus-upload-time.sh
-            sed -i 's#patch#${nexusPatch}#g' nexus-upload-time.sh
             sed -i 's/one/${nexusname1}/g' nexus-upload-time.sh
             sed -i 's/two/${nexusname2}/g' nexus-upload-time.sh
             sed -i 's/id/${nexusname1}-${nexusname2}/g' nexus-upload-time.sh
             sed -i 's/type/${packagetype}/g' nexus-upload-time.sh
             sh nexus-upload-time.sh
+             """
+       }catch (e){
+            currentBuild.description="制品上传失败!"
+            error '制品上传失败!'
+        }
+}
+
+def nexusTimeBook(packagetype,jar,nexusRep,nexusGroup,nexusPatch,nexusname1,nexusname2,book){
+    try{
+          sh """
+            echo ${jar}
+            cp /data/build-devops/nexus-common/nexus-upload-time1.sh $workspace
+            sed -i 's#name#${jar}#g' nexus-upload-time1.sh
+            sed -i 's#book#${book}#g' nexus-upload-time1.sh
+            sed -i 's#cangku#${nexusRep}#g' nexus-upload-time1.sh
+            sed -i 's#group#${nexusGroup}#g' nexus-upload-time1.sh
+            sed -i 's#patch#${nexusPatch}#g' nexus-upload-time1.sh
+            sed -i 's/one/${nexusname1}/g' nexus-upload-time1.sh
+            sed -i 's/two/${nexusname2}/g' nexus-upload-time1.sh
+            sed -i 's/id/${nexusname1}-${nexusname2}/g' nexus-upload-time1.sh
+            sed -i 's/type/${packagetype}/g' nexus-upload-time1.sh
+            sh nexus-upload-time1.sh
              """
        }catch (e){
             currentBuild.description="制品上传失败!"
