e17bc4b25f7c6b53720cb025e310b0ba09ed94bd
陈景涛 <cjt@youlu.com>
2020-10-19 16:16:48 +0800

    Update youluSvnWar.groovy

--- vars/youluSvnWar.groovy
+++ vars/youluSvnWar.groovy
@@ -82,7 +82,7 @@ pipeline {
                       } 
                 }
             steps {
-            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'renshuaipeng', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "http://171.15.16.189:11080/svn/youlu/MicroService/${PROJECT_VERSION}/BaseData"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'chenjingtao-svn', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "http://171.15.16.189:11080/svn/youlu/MicroService/${PROJECT_VERSION}/BaseData"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
             }
         }
         stage('部署服务'){
