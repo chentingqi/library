2b55519ae4b962429eb589c74f72040f5e437453
陈景涛 <cjt@youlu.com>
2020-10-20 17:41:57 +0800

    Update youluSvnWar.groovy

--- vars/youluSvnWar.groovy
+++ vars/youluSvnWar.groovy
@@ -80,7 +80,7 @@ pipeline {
         stage('单元测试'){
             when { anyOf { environment name: 'ENV', value: 'dev' } }
             steps {
-            sh "/data/maven/apache-maven-3.6.2/bin/mvn test"
+            sh "/data/maven/apache-maven-3.6.2/bin/mvn test -Dmaven.repo.local=/data/.repository"
             
         }
         }
