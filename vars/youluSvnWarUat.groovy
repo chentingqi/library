c9a3def4c65a52bd96b3c406d1de7963c8080582
陈景涛 <cjt@youlu.com>
2020-10-23 18:20:54 +0800

    Update youluSvnWarUat.groovy

--- vars/youluSvnWarUat.groovy
+++ vars/youluSvnWarUat.groovy
@@ -46,8 +46,10 @@ pipeline {
                       } 
                 }
             steps {
+            script {
             if (params.PROJECT_VERSION != "null") {
             checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[cancelProcessOnExternalsFail: true, credentialsId: 'chenjingtao-svn', depthOption: 'infinity', ignoreExternalsOption: true, local: '.', remote: "http://171.15.16.189:11080/svn/youlu/MicroService/${PROJECT_VERSION}/${map.NEXUS_NAME}/${PROJECT_PATCH}"]], quietOperation: true, workspaceUpdater: [$class: 'UpdateUpdater']])
+            }
             }
             }
         }
