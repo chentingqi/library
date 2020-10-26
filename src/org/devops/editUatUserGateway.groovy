29e144a9cbaa59cbe7148ef24f45087710750a64
陈景涛 <cjt@youlu.com>
2020-10-26 14:06:51 +0800

    Update editUatUserGateway.groovy

--- src/org/devops/editUatUserGateway.groovy
+++ src/org/devops/editUatUserGateway.groovy
@@ -1,16 +1,7 @@
 package org.devops
 
 def activitycenter(service){
-    if (service == '172.31.129.84'){
-       try{
-           sh """
-              sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.84 8330 activitycenter
-           """
-        }catch(e){
-             currentBuild.description="修改网关配置${service}失败!"
-             echo "修改网关配置${service}失败!"
-        }
-    }
+    if (service == '172.31.129.84'){try{sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.84 8330 activitycenter"""}catch(e){currentBuild.description="修改网关配置${service}失败!"echo "修改网关配置${service}失败!"}}
     if (service == '172.31.129.79'){
        try{
            sh """
