977c543a373cc59527cde8a30c8693f9cb2eb435
陈景涛 <cjt@youlu.com>
2020-10-26 15:11:08 +0800

    Update editUatUserGateway.groovy

--- src/org/devops/editUatUserGateway.groovy
+++ src/org/devops/editUatUserGateway.groovy
@@ -6,4 +6,9 @@ def editUatUserGateway(service,ip){
        if (ip == 'IP2'){sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.79 8330 activitycenter"""}
        if (ip == 'IP12'){sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.79 172.31.129.84 8330 activitycenter"""}
     }
+    if (service == 'basedata'){
+       if (ip == 'IP1'){sh """sh /data/GATEWAY/user_edit_fgateway.sh bd.url 172.31.129.84 8300 basedata"""}
+       if (ip == 'IP2'){sh """sh /data/GATEWAY/user_edit_fgateway.sh bd.url 172.31.129.79 8300 basedata"""}
+       if (ip == 'IP12'){sh """sh /data/GATEWAY/user_edit_fgateway.sh bd.url 172.31.129.79 172.31.129.84 8300 basedata"""}
+    }
 }
