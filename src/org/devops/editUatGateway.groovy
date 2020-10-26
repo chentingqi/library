5e5ed4e1f9bec35b9d777482097722bf4e5ee688
陈景涛 <cjt@youlu.com>
2020-10-26 15:53:02 +0800

    Update editUatGateway.groovy

--- src/org/devops/editUatGateway.groovy
+++ src/org/devops/editUatGateway.groovy
@@ -1,6 +1,6 @@
 package org.devops
 
-def editUatAdminGateway(service,ip){
+def editUatGateway(service,ip){
     if (service == 'activitycenter'){
        if (ip == 'IP1'){sh """sh /data/GATEWAY/uat_edit_fgateway.sh ac.url 172.31.129.84 8330 activitycenter"""}
        if (ip == 'IP2'){sh """sh /data/GATEWAY/uat_edit_fgateway.sh ac.url 172.31.129.79 8330 activitycenter"""}
