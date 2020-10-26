0390c10c992a4ef85a66a1fb112aec6ffc05b82a
陈景涛 <cjt@youlu.com>
2020-10-26 14:49:28 +0800

    Update editUatUserGateway.groovy

--- src/org/devops/editUatUserGateway.groovy
+++ src/org/devops/editUatUserGateway.groovy
@@ -2,19 +2,8 @@ package org.devops
 
 def editUatUserGateway(service,ip){
     if (service == 'activitycenter'){
-       if (ip == '172.31.129.84'){
-           try{
-               sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.84 8330 activitycenter"""
-               }
-        }
-       if (ip == '172.31.129.79'){
-           try{
-               sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.79 8330 activitycenter"""   
-           }
-       }
-       if (ip == '172.31.129.79-172.31.129.84'){
-           try{sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.79 172.31.129.84 8330 activitycenter"""
-           }
-       }
+       if (ip == '172.31.129.84'){sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.84 8330 activitycenter"""}
+       if (ip == '172.31.129.79'){sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.79 8330 activitycenter"""}
+       if (ip == '172.31.129.79-172.31.129.84'){sh """sh /data/GATEWAY/user_edit_fgateway.sh ac.url 172.31.129.79 172.31.129.84 8330 activitycenter"""}
     }
 }
